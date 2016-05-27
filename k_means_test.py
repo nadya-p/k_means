f1f46990be847c5e42abece75056b39ad558bdfa
nadya-p <qwertyqwerty987322@gmail.com>
2016-05-27 10:50:48 +0200

    unit-tested the comparison with sklearn k-mean

--- k_means_test.py
+++ k_means_test.py
@@ -1,7 +1,9 @@
 import k_means
 import numpy as np
+import sklearn.cluster
 import numpy.testing
 from sklearn.utils.testing import assert_raise_message
+from sklearn.utils import check_random_state
 
 
 def no_points():
@@ -36,9 +38,35 @@ def separate_center():
     numpy.testing.assert_array_equal(centers.tolist(), [[0, 2], [0, 10]])
 
 
+def compare_to_sklearn():
+    n_dim = 2
+    n_clusters = 3
+    n_samples = 5
+    random_state = check_random_state(0)
+    X = np.ndarray([n_clusters*n_samples, n_dim])
+    initial_centers = random_state.rand(n_clusters, n_dim)*10
+    for i in range(0, n_clusters):
+        x_current = random_state.multivariate_normal(initial_centers[i, :], [[1, 0], [0, 1]], n_samples)
+        X[i*n_samples:(i+1)*n_samples, :] = x_current
+    closest_center, initial_centers = k_means.k_means(X, n_clusters)
+    k_means_scikit_learn = sklearn.cluster.KMeans(n_clusters=n_clusters, random_state=random_state)
+    closest_center_sklearn = k_means_scikit_learn.fit_predict(X)
+    _swap_values_in_ndarray(closest_center_sklearn, 2, 0)
+    numpy.testing.assert_array_equal(closest_center, closest_center_sklearn)
+
+
+def _swap_values_in_ndarray(x, val1, val2):
+    i_val1 = x == val1
+    i_val2 = x == val2
+    x[i_val1] = -1
+    x[i_val2] = val1
+    x[i_val1] = val2
+
+
 if __name__ == '__main__':
     no_points()
     one_point()
     two_points()
     four_points()
     separate_center()
+    compare_to_sklearn()
